085576e150a532a8dbef9241ec40188a0f4eac97
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-16 20:00:01 +0000

    Creating branch for BioC 3.1 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/DOSE@102593 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/AllGenerics.R
@@ -0,0 +1,78 @@
+##' cnetplot method generics
+##'
+##'
+##' @docType methods
+##' @name cnetplot
+##' @rdname cnetplot-methods
+##' @title cnetplot method
+##' @return plot
+##' @export
+##' @author Guangchuang Yu \url{http://ygc.name}
+## if ( !isGeneric("cnetplot") )
+setGeneric("cnetplot", function(x, showCategory=5, categorySize="geneNum", foldChange=NULL, fixed=TRUE,...) standardGeneric("cnetplot"))
+
+
+## ## @exportMethod "setReadable<-"
+## setGeneric(
+##            name="setReadable<-",
+##            def=function(x, value) {standardGeneric("setReadable<-")}
+##            )
+
+##' preparing geneSets for gene set enrichment analysis
+##'
+## @S3method getGeneSet DO
+##' @title getGeneSet
+##' @param setType type of gene sets
+##' @param organism organism
+##' @param ... additional parameter
+##' @export
+getGeneSet <- function(setType, organism, ...) {
+    UseMethod("getGeneSet")
+}
+
+##' Mapping External ID to Ontology Term ID
+##'
+## @S3method EXTID2TERMID DO
+##' @title EXTID2TERMID
+##' @param gene gene ID vector
+##' @param organism organism
+##' @param ... additional parameter
+##' @export
+EXTID2TERMID <- function(gene, organism, ...) {
+    UseMethod("EXTID2TERMID")
+}
+
+##' Mapping Ontology Term ID to External ID
+##'
+## @S3method TERMID2EXTID DO
+##' @title TERMID2EXTID
+##' @param term term ID vector
+##' @param organism organism
+##' @param ... additional parameter
+##' @export
+TERMID2EXTID <- function(term, organism, ...) {
+    UseMethod("TERMID2EXTID")
+}
+
+##' Get all background External ID.
+##'
+## @S3method ALLEXTID DO
+##' @title ALLEXTID
+##' @param organism organism
+##' @param ... additional parameter
+##' @export
+ALLEXTID <- function(organism, ...) {
+    UseMethod("ALLEXTID")
+}
+
+##' Mapping Ontology Term ID to Name Symbol or Description
+##'
+## @S3method TERM2NAME DO
+##' @title TERM2NAME
+##' @param term term ID vector
+##' @param organism organism
+##' @param ... additional parameter
+##' @export
+TERM2NAME <- function(term, organism, ...) {
+    UseMethod("TERM2NAME")
+}
